60ff28f4ad983f90d34e82a5dee84a711cbad682
samatha_ramakrishna@yahoo.com <samatha_ramakrishna@yahoo.com>
2024-01-26 18:14:03 +0530

    Add project files.

---
+++ AppointmentProject/Appointment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentProject
+{
+  internal  class Appointment
+    {
+        public string Day { get; }
+        public int Time { get; }
+
+        public Appointment(string day, int time)
+        {
+            Day = day;
+            Time = time;
+        }
+
+        public string CheckSlot()
+        {
+            Dictionary<string, int> timeSlots = GetTimeSlots();
+
+            if (timeSlots.ContainsKey(Day) && Time >= 8 && Time < timeSlots[Day])
+            {
+                return "Available";
+            }
+            else
+            {
+                return "Not Available";
+            }
+        }
+
+        private Dictionary<string, int> GetTimeSlots()
+        {
+            Dictionary<string, int> timeSlots = new Dictionary<string, int>
+        {
+            {"Monday", 17},
+            {"Tuesday", 16},
+            {"Wednesday", 14},
+            {"Thursday", 12},
+            {"Friday", 10},
+            {"Saturday", 0}, // No appointments on Saturday
+            {"Sunday", 0}    // No appointments on Sunday
+        };
+
+            return timeSlots;
+        }
+    }
+}
---
+++ AppointmentProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Sample Input
+using AppointmentProject;
+
+Appointment obj = new Appointment("Tuesday", 15);
+var res = obj.CheckSlot();
+
+// Sample Output
+Console.WriteLine(res); // Output: Available
